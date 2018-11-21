bb98fb875e8c59a383e589617531ed2f67bfc176
Cody Lynn <jclynn87@gmail.com>
2018-11-21 14:31:49 -0500

    Launch Page Background Done

---
+++ src/containers/Launch/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import './launch.css';
+
+export default class Launch extends Component {
+  render() {
+    return (
+      <div>
+        <div id="block1">   
+        </div>
+        <div id="block2">   
+        </div>
+        <div id="block3">
+        </div>
+        <div id="block4">
+        </div>
+        <div id="block5">   
+        </div>
+        <div id="block6">   
+        </div>
+        <div id="block7">
+        </div>
+        <div id="block8">
+        </div>
+      </div>
+    )
+  }
+}
