2b06f0c2e5b950eef65c86bb0a3beb628e0b2627
Cody Lynn <jclynn87@gmail.com>
2018-11-21 20:55:02 -0500

    Beginnings of Launch Copy with2 blocks

--- src/containers/Launch/index.js
+++ src/containers/Launch/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
 import './launch.css';
 
 export default class Launch extends Component {
   render() {
     return (
       <div>
+        <Header />
         <div id="block1">   
         </div>
         <div id="block2">   
@@ -13,14 +16,21 @@ export default class Launch extends Component {
         </div>
         <div id="block4">
         </div>
-        <div id="block5">   
+        <div id="block5">
+          <p><strong>TEMPORARY STAFFING</strong></p>   
         </div>
-        <div id="block6">   
+        <div id="btn_block">
+          <p>See the difference <strong>TAG</strong> can make in your office.</p>
+          <button>Let's Get Started</button>
         </div>
         <div id="block7">
+          <p><strong>HIRING EMPLOYERS</strong></p>
         </div>
-        <div id="block8">
+        <div id="btn_block">
+          <p><strong>TAG</strong> makes finding staff simple.</p>
+          <button>Let's Get Started</button>
         </div>
+        <Footer />
       </div>
     )
   }
