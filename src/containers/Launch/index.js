e675ff4c69dbafcc5ab60131cf1006463c303c60
Cody Lynn <jclynn87@gmail.com>
2018-11-30 12:04:23 -0500

    Main Logo Block Finished and Fonts Added

--- src/containers/Launch/index.js
+++ src/containers/Launch/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import './launch.css';
-import logo from '../../imgs/logo_blue.png';
+
 
 export default class Launch extends Component {
   render() {
@@ -10,25 +10,70 @@ export default class Launch extends Component {
       <div>
         <Header />
         <div id="block1"> 
-          {/*<img src={logo} alt="Main Color Logo" />*/}
+          <div id="hero">
+            <img src={require('../../imgs/logo_blue.png')} alt="Main Color Logo" />
+            <h1><strong>TEMPORARY ASSISTANCE GURU</strong></h1>
+            <p>Connecting temp agencies to hiring businesses.</p>
+          </div>
+          <div>
+            <button>I'M A TEMP AGENCY</button>
+            <button>I WANT TO HIRE</button>
+          </div>
         </div>
         <div id="block2">  
           <p><strong>DOWNLOAD THE APP</strong></p> 
           <div id="copy_txt"><strong>TAG</strong> makes it easy to find temps from multiple agencies in one place. Download the app and get to work!</div>
+          <div>
+            <img src={require('../../imgs/apple.svg')} alt="Apple Store Link" />
+            <img src={require('../../imgs/google.png')} alt="Google Play Store Link" />
+          </div>
         </div>
         <div id="block3">
         </div>
         <div id="block4">
         </div>
         <div id="block5">
-          <p><strong>TEMPORARY STAFFING</strong></p>   
+          <h3><strong>TEMPORARY STAFFING</strong></h3>
+          <div> 
+            <img src={require('../../imgs/phone_notify.png')} alt="Phone Icon" />
+            <p>Cut down on sales cost by having orders sent directly to your phone.</p>
+          </div>
+          <div> 
+            <img src={require('../../imgs/building_loc.png')} alt="Building Icon" />
+            <p>Get multiple orders daily by companies in need in your immediate area.</p>
+            
+          </div>  
+          <div> 
+            <img src={require('../../imgs/clipboard_gear.png')} alt="Clipboard Icon" />
+            <p>Manage and accept complete or partial orders and see credit reports and billing information.</p>
+          </div>  
+          <div> 
+            <img src={require('../../imgs/message_icon.png')} alt="Chat Icon" />
+            <p>Chat feature allows questions to be asked and answered daily.</p>
+          </div>  
         </div>
         <div id="btn_block">
           <p>See the difference <strong>TAG</strong> can make in your office.</p>
           <button>Let's Get Started</button>
         </div>
         <div id="block7">
-          <p><strong>HIRING EMPLOYERS</strong></p>
+          <h3><strong>HIRING EMPLOYERS</strong></h3>
+          <div> 
+            <img src={require('../../imgs/building_loc.png')} alt="Building Icon" />
+            <p>Easily access a city's worth of temporary workers with the click of a button. Bill rate is set by city, no more negotiating bill rates.</p>
+          </div>
+          <div> 
+            <img src={require('../../imgs/clipboard_check.png')} alt="Clipboard Icon" />
+            <p>Place one order and multiple companies can accept in real time.</p> 
+          </div>  
+          <div> 
+            <img src={require('../../imgs/stars.png')} alt="Stars Icon" />
+            <p>Agencies are rated to ensure quality, quantity, and timeliness.</p>
+          </div>  
+          <div> 
+            <img src={require('../../imgs/message_icon.png')} alt="Chat Icon" />
+            <p>Chat feature allows questions to be asked and answered in the app, without phone numbers being distributed.</p>
+          </div>  
         </div>
         <div id="btn_block">
           <p><strong>TAG</strong> makes finding staff simple.</p>
