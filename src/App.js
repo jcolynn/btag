6152ff44d0271a30a305f622eea8e775af3150bd
Cody Lynn <jclynn87@gmail.com>
2018-12-10 14:49:31 -0500

    Small CSS change

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Launch from './containers/Launch';
 import NotFound from './containers/NotFound';
 import Contact from './containers/Contact';
 import Login from './containers/Login';
-import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Router>
           <Launch path="/" />
           <NotFound default />
