8f1135282fd5a2d460c8212c758288b2bd9763f9
Cody Lynn <jclynn87@gmail.com>
2018-12-12 12:50:26 -0500

    Responsive Fixes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,46 +1,49 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 
 import "./footer.css";
 
 export default function Footer() {
   return (
     <div>
-      <footer>  
-        <div className="top">
-          {/* Left Links */}
-          <div className="link_box">
-            <p id="links">Create an Account</p>
-            <p id="links">How TAG Works</p>
-            <p id="links">Contact Us</p>
-          </div>
-          
-          {/* Footer Logo */}
-          <div className="logo_box">
-            <img className="logo" src={require('../../imgs/logo_white.png')} alt="logo" />
-            <div className="logo_text">TAG</div>
-            <div className="name">Temporary Assistance Guru</div>
+      <MediaQuery minWidth={960} >
+        <footer>  
+          <div className="top">
+            {/* Left Links */}
+            <div className="link_box">
+              <p id="links">Create an Account</p>
+              <p id="links">How TAG Works</p>
+              <p id="links">Contact Us</p>
+            </div>
+            
+            {/* Footer Logo */}
+            <div className="logo_box">
+              <img className="logo" src={require('../../imgs/logo_white.png')} alt="logo" />
+              <div className="logo_text">TAG</div>
+              <div className="name">Temporary Assistance Guru</div>
+            </div>
+
+            {/* Social Icon Box */}
+            <div className="social_box">
+              <span id="social_icon">  
+                <i className="fab fa-facebook"></i>
+              </span>
+              <span id="social_icon">  
+                <i className="fab fa-twitter-square"></i>
+              </span>
+              <span id="social_icon">  
+                <i className="fab fa-linkedin"></i>
+              </span>
+            </div>
           </div>
 
-          {/* Social Icon Box */}
-          <div className="social_box">
-            <span id="social_icon">  
-              <i className="fab fa-facebook"></i>
-            </span>
-            <span id="social_icon">  
-              <i className="fab fa-twitter-square"></i>
-            </span>
-            <span id="social_icon">  
-              <i className="fab fa-linkedin"></i>
-            </span>
+          {/* Copyright Info Footer */}
+          <div className="bottom">
+            <span className="copyright"><i className="far fa-copyright"></i></span>
+            <span className="foot_font"> 2018, Temporary Assistance Guru. All Rights Reserved. | Terms of Use | Privacy Policy</span>
           </div>
-        </div>
-
-        {/* Copyright Info Footer */}
-        <div className="bottom">
-          <span className="copyright"><i className="far fa-copyright"></i></span>
-          <span className="foot_font"> 2018, Temporary Assistance Guru. All Rights Reserved. | Terms of Use | Privacy Policy</span>
-        </div>
-      </footer>
+        </footer>
+      </MediaQuery>
     </div>
   )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,14 +6,9 @@ export default function Header() {
   return (
     <div> 
       <header>  
-        <div id="left_links">
-          <a href="http://localhost:3000" className="header_link">HOME</a>
-          <a href="http://localhost:3000" className="header_link">CONTACT US</a>
-        </div>
+
         <img className="head_logo" src={require("../../imgs/logo_white.png")} alt="White Colored Logo"/>
-        <div id="right_links">
-          <a href="http://localhost:3000" className="header_link">LOGIN</a>
-        </div>
+
       </header>
     </div>
   )
