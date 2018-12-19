7deb9915a1edc64fa1dbf6d9928123fd1eab43ed
Cody Lynn <jclynn87@gmail.com>
2018-12-19 18:57:06 -0500

    Contact Page Setup

--- src/containers/Contact/index.js
+++ src/containers/Contact/index.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
-
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import './contact.css';
 export default class Contact extends Component {
   render() {
     return (
       <div>
-        Contact Page
+        <Header />
+        <div class="page">
+          <div id="inputform">
+            <form>
+              <input type="text" placeholder="To" name="To" />
+              <input type="text" placeholder="Subject" name="Subject" />
+              <label for="description">Description</label>
+              <input id="description" type="textarea" />
+            </form>
+          </div>
+          <div id="contactdetails">
+            <h1>CONTACT</h1>
+            <span>Email: tag@email.com</span>{" "}
+            <span>Phone: (098)765-4321</span>
+          </div>
+        </div>
+        <Footer />
       </div>
     )
   }
